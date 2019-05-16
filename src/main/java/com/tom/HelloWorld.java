4ecb24ab99e040865c1a6e3b5b729f960cdfb715
stephenhu <i75407263@gmail.com>
2019-05-16 22:21:47 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/tom/HelloWorld.java
+++ src/main/java/com/tom/HelloWorld.java
@@ -5,5 +5,15 @@ public class HelloWorld {
 //        System.out.println("Hello World");
         Person person = new Person();
         person.hello();
+        int age = 30;
+        Integer age2 = 31;
+        char c = 'Q';
+        Character c2 = 'X';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
